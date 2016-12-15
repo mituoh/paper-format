69959648c5a983f58357ad1d6c6fbcf3afd4bb54
Hironori Inagaki <lego.nori@gmail.com>
2016-12-15 20:40:28 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <format></format>
+  </div>
+</template>
+
+<script>
+import Format from './components/Format';
+
+export default {
+  name: 'app',
+  components: {
+    Format,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
